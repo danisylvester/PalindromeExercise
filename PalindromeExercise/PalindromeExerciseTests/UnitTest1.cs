5ed83a1cb0c02ca36a59d5cf1e8bc1e3a04edc04
danisylvester <maria.sylvester10@gmail.com>
2020-09-15 10:32:48 -0500

    Created a method to see if an input is a palindrome & a unit test to test the method.

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("civic", true)]
+        [InlineData("cat", false)]
+        [InlineData("stats", true)]
+        public void IsAPalindrome(string input, bool expected)
         {
-
+            var p = new WordSmith();
+            bool actual = p.IsAPalindrome(input);
+            Assert.Equal(actual, expected);
         }
     }
 }
